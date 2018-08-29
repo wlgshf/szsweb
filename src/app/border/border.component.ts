3db65d0317dd8cb1876b2cfae0e077d0ca0c948c
song chao <dlhtsc@126.com>
2018-08-29 17:53:18 +0800

    v1.6

---
+++ src/app/border/border.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-border',
+  templateUrl: './border.component.html',
+  styleUrls: ['./border.component.css']
+})
+export class BorderComponent implements OnInit {
+  array = [1, 2, 3, 4];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+
+}
